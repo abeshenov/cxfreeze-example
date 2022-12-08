c0c2fcd0c06711189cb74f8631a7498686490854
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-12-08 12:35:52 -0600

    black

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import numpy
 
 # Just some random code that uses numpy:
 def calculate_pi(n):
-    points = numpy.random.uniform(0, 1, size=(n,2))
-    points_in_unit_circle = numpy.sqrt(points[:,0]**2 + points[:,1]**2) < 1
-    point_count = sum(points_in_unit_circle).astype('double')
+    points = numpy.random.uniform(0, 1, size=(n, 2))
+    points_in_unit_circle = numpy.sqrt(points[:, 0] ** 2 + points[:, 1] ** 2) < 1
+    point_count = sum(points_in_unit_circle).astype("double")
 
     return point_count / n * 4
 
